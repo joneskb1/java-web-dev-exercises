c955b3d6845da36a58990554b425aca6a53bdcc1
Kayla Jones <kayla3445@yahoo.com>
2021-01-10 15:16:28 -0600

    updated alice

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -1,4 +1,5 @@
 package exercises;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Alice {
@@ -7,14 +8,14 @@ public class Alice {
         String sentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
         Scanner input = new Scanner(System.in);
         System.out.println("enter a word");
+        String quote = sentence.toLowerCase();
         String searchTerm = input.nextLine();
-        boolean contains = sentence.toLowerCase().contains(searchTerm);
-        System.out.println(contains);
+        searchTerm = searchTerm.toLowerCase();
         int indexOfSearch = sentence.indexOf(searchTerm);
         int lengthSearch = searchTerm.length();
         System.out.println(indexOfSearch);
         System.out.println(lengthSearch);
-        String newString = sentence.replace(searchTerm, "");
+        String newString = quote.replaceFirst(searchTerm, "");
         System.out.println(newString);
 
 
